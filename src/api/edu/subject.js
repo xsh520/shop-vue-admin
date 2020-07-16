b9aa4d8bb2639376331b4e710dd1da23d72c6c2d
xsh520 <954186642@qq.com>
2020-07-16 22:33:22 +0800

    课程管理

--- src/api/edu/subject.js
+++ src/api/edu/subject.js
@@ -3,7 +3,7 @@ export default {
     //1 课程分类列表
     getSubjectList() {
         return request({
-            url: '/eduservice/subject/list',
+            url: '/eduservice/subject/true',
             method: 'get'
           })
     }
